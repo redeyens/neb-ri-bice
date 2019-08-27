c8069342de1d297f598c6513ffbeeb7e50cb3fc8
redeyens <54171005+redeyens@users.noreply.github.com>
2019-08-27 11:07:48 +0200

    Added actual thickness lookup

--- examples/Example2/Program.cs
+++ examples/Example2/Program.cs
@@ -9,28 +9,47 @@ namespace Example2
 
         static void Main()
         {
+            double[] availableGlassPanelThickness = new double[] { 4.0, 6.0, 8.0, 10.0, 12.0, 15.0, 20.0, 30.0, 40.0, 50.0 };
+
+
             double width = GetInput("Enter width [mm]: ");
             double length = GetInput("Enter length [mm]: ");
             double waterLevel = GetInput("Enter water level (height) [mm]: ");
 
             double betaBottomPanel = LookupBetaForBottomPanel(length, width);
-            double bottomThickness = GlassThickness(waterLevel, betaBottomPanel);
+            double bottomMinimumThickness = GlassThickness(waterLevel, betaBottomPanel);
+            double bottomThickness = GetAvailableGlassThickness(bottomMinimumThickness, availableGlassPanelThickness);
 
             double betaFrontPanel = LookupBetaForSidePanel(length, waterLevel);
-            double frontPanelThickness = GlassThickness(waterLevel, betaFrontPanel);
+            double frontPanelMinimumThickness = GlassThickness(waterLevel, betaFrontPanel);
+            double frontPanelThickness = GetAvailableGlassThickness(frontPanelMinimumThickness, availableGlassPanelThickness);
 
             double betaSidePanel = LookupBetaForSidePanel(width, waterLevel);
-            double sidePanelThickness = GlassThickness(waterLevel, betaSidePanel);
+            double sidePanelMinimumThickness = GlassThickness(waterLevel, betaSidePanel);
+            double sidePanelThickness = GetAvailableGlassThickness(sidePanelMinimumThickness, availableGlassPanelThickness);
 
             Console.WriteLine();
-            Console.WriteLine($"Required glass thickness for bottom panel is {bottomThickness:N1} mm.");
-            Console.WriteLine($"Required glass thickness for front/back panel is {frontPanelThickness:N1} mm.");
-            Console.WriteLine($"Required glass thickness for left/right panel is {sidePanelThickness:N1} mm.");
+            Console.WriteLine($"Required glass thickness for bottom panel is {bottomThickness:N1} mm, minimum was {bottomMinimumThickness:N1} mm.");
+            Console.WriteLine($"Required glass thickness for front/back panel {frontPanelThickness:N1} mm, minimum was is {frontPanelMinimumThickness:N1} mm.");
+            Console.WriteLine($"Required glass thickness for left/right panel {sidePanelThickness:N1} mm, minimum was is {sidePanelMinimumThickness:N1} mm.");
 
             Console.WriteLine();
             Console.WriteLine($"Total glass area is {TotalGlassAreaMeters(width, length, waterLevel):N1} m^2.");
             Console.WriteLine($"Total water volume is {WaterVolumeLiters(width, length, waterLevel):N1} l.");
 
+        }
+
+        private static double GetAvailableGlassThickness(double minimumThickness, double[] availableGlassPanelThickness)
+        {
+            for (int i = 0; i < availableGlassPanelThickness.Length; i++)
+            {
+                if (availableGlassPanelThickness[i] >= minimumThickness)
+                {
+                    return availableGlassPanelThickness[i];
+                }
+            }
+
+            return 0;
         }
 
         private static object TotalGlassAreaMeters(double width, double length, double waterLevel)
